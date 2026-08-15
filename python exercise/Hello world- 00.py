56d192ff75da0a3de07309b203a8af1cb4e674a1
Prashanth N C <prashanthnc4@gmail.com>
2026-08-15 22:38:44 +0530

    Add milestone comments to Hello World program
    Added comments to celebrate the first Python program.

--- python exercise/Hello world- 00.py
+++ python exercise/Hello world- 00.py
@@ -1,1 +1,8 @@
+#🚀 First milestone in my Python journey!
+# I just executed my very first “Hello, World!” program in Python 🐍💻
+# It may be a simple program, but every great journey starts with a first step. Excited to keep learning, building, and improving! 🌱
+
+
+
+
 print("Hello, World!")
