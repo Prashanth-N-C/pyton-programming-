c757ad3fd0cd775f9bb8dde8d2abf2e74ae4d5cb
Prashanth N C <prashanthnc4@gmail.com>
2026-08-16 21:04:54 +0530

    variables  in python

---
+++ python exercise/01- VARIABLES.py
@@ -0,0 +1,21 @@
+#What is a Variable?
+#A variable is a named container that stores a value. Think of it like a labeled box where you put information.
+
+#Box labeled "name" contains → "Alice"
+#Box labeled "age" contains → 25
+#Box labeled "height" contains → 5.9
+
+
+
+#name → Variable name (the label on the box)
+#= → Assignment operator (put something in the box)
+#"Alice" → The value (what goes in the box)
+
+
+name = "Alice"
+age = 25
+height = 5.9
+
+print(name)
+print(age )
+print(height)
