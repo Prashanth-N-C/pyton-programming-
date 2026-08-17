472896dc13ded94cc70f4eee7e1124c7c56cb5bd
Prashanth N C <prashanthnc4@gmail.com>
2026-08-17 16:15:02 +0530

    Add files via upload

---
+++ python exercise/02-data types.py
@@ -0,0 +1,15 @@
+ #what is data type?
+#A data type tells Python what kind of data a value is.
+#String — str
+#Integer — int
+#Float — float
+#Boolean — bool
+
+name = "prashanth"
+age = 18
+height = 6.1
+is_student = True
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
