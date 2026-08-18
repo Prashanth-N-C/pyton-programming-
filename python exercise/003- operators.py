3ccd7ed6f2d800f834444c9b627bf8f073b1ff2f
Prashanth N C <prashanthnc4@gmail.com>
2026-08-18 15:10:40 +0530

    Add files via upload

---
+++ python exercise/003- operators.py
@@ -0,0 +1,19 @@
+#What are Operators in Python?
+#perators are special symbols or keywords used to perform operations on values and variables
+
+#Main types of operators in Python
+#1. Arithmetic Operators
+#2. Comparison Operators
+#3. Assignment Operators
+#4. Logical Operators
+
+
+a = 15
+b = 10
+print(a+b)
+print(a>b)
+x = 10
+x +=5
+print(x)
+age = 20
+print(age >18 and age< 30)
