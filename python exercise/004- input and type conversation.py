bb7f6e40969ba2e736f0001ecf2dead93b740fa3
Prashanth N C <prashanthnc4@gmail.com>
2026-08-19 23:53:45 +0530

    Add files via upload

---
+++ python exercise/004- input and type conversation.py
@@ -0,0 +1,23 @@
+#What is Input in Python?
+#Input means taking information from the user while the program is running.
+
+#What is Type Conversion?
+#Type conversion means changing a value from one data type to another.
+
+name = input("Enter your name:")
+print("Hello",name)
+
+
+
+age ="20" 
+age = int(age)
+print(age)
+print(type(age))
+
+num1 = input("enter first number:")
+num2 = input("Enter second number:")
+print(num1+num2)
+
+num1 =int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+print(num1+num2)
