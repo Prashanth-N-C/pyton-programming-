da66495305a9cf5e0fa310338d7f7445254f27bb
Prashanth N C <prashanthnc4@gmail.com>
2026-08-20 18:20:31 +0530

    Add files via upload

---
+++ python exercise/05-conditional statments in python.py
@@ -0,0 +1,30 @@
+age = 18
+if age>= 18:
+    print("you are eligible to vote")
+
+age = 16
+if age>=18:
+    print("you can vote")
+else:
+    print("you cannot vote")
+
+    marks = 75
+    if marks>=90:
+        print("Grade A")
+
+    elif marks>=60:
+        print("Grande B")
+    else:
+        pritn("Fail")
+
+
+    age = 20 
+    has_id = True
+
+    if age>=18:
+        if has_id:
+            print("Entry allowed")
+        else:
+            print("ID required")
+    else:
+        print("Entry not allowed")
