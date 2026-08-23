b5bf74a2543f817a2fabb7a3d2521c5808d055bb
Prashanth N C <prashanthnc4@gmail.com>
2026-08-23 21:58:30 +0530

    Add files via upload

---
+++ python exercise/06-off loop.py
@@ -0,0 +1,88 @@
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+for i in range(1,6):
+    print(i)
+
+for i in range(5):
+    print(i)
+
+for  i  in range(1,19):
+    print(i)
+
+for i in range(2,23,2):
+    print(i)
+
+
+for i in range(10,0,-1):
+    print(i)
+
+
+
+name ="python"
+
+for letter in name:
+    print(letter)
+
+name = "prashanth"
+
+for letter in name:
+    print(letter)
+
+
+fruits = ["apple","banana","oranges"]
+
+for fruit in fruits:
+    print(fruit)
+
+for i in range(2,21,2):
+    print(i)
+
+
+for i in range(1,21,2):
+    print(i)
+
+
+for i in range(10):
+    print("PASHANTH")
+
+
+for i in range(5):
+    print(5*1)
+    print(5*2)
+    print(5*3)
+
+
+num = int(input("Enter a number:"))
+
+for i in range(1,11):
+    print(num,"x",i,"=",num*i)
+
+num = int(input("Enter a number:"))
+
+for i in range(1,11):
+    print(num,"x",i,"=",num*i)
+
+
+total = 0
+
+for i in range(1,101):
+    total +=i
+
+print(total)
+
+
+for i in range(1,6):
+    print("*"*i)
+
+
+num = int(input("Enter a number:"))
+
+factorial = 1
+for i in range(1,num+1):
+    factorial = factorial*i
+
+print("Factorial =",factorial)
