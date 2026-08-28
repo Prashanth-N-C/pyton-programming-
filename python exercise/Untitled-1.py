546d1cd323ba7f16967385e3cf459b0ddb866858
Prashanth N C <prashanthnc4@gmail.com>
2026-08-28 22:57:07 +0530

    Add files via upload

---
+++ python exercise/Untitled-1.py
@@ -0,0 +1,20 @@
+fruits = ["apple","banana","mango"]
+
+print(fruits[0])
+fruits[1] = "orange"
+print(fruits) 
+
+
+colors = ("orange ","green","blue")
+print(colors[1])
+colors[1] = red
+
+
+numbers = {10,20,30,10,20}
+print(numbers)
+
+student = {"name" = pranstanth , "age" = 26 , "height"=9.6}
+print(student[name])
+
+student = {"name": "Prashanth","age": 20,"course": "Python"}
+print(student["name"])
